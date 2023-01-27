5e7c1c873fae480555bef10f265edbb3e89c06b3
tkdgh7063 <hoo412298@gmail.com>
2023-01-27 19:52:45 +0900

    Express branch merge

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routes/globalRouter";
+import userRouter from "./routes/userRouter";
+import videoRouter from "./routes/videoRouter";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("views", process.cwd() + "/src/views");
+app.set("view engine", "pug");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
