70926f538f44ce8b7354d0e75ede4eb239593f71
tkdgh7063 <hoo412298@gmail.com>
2023-01-29 23:42:50 +0900

    Usage of Morgan
    Morgan is middleware package returning response depending on format option

--- src/server.js
+++ src/server.js
@@ -1,33 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
   return res.send("Response");
 };
-const handleProtected = (req, res) => {
-  return res.send("Welcome to private lounge.");
+const login = (req, res) => {
+  return res.send("Login");
 };
 
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}`);
