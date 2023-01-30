52938823c2c0d4007353681a863f4d6c54e92af5
tkdgh7063 <hoo412298@gmail.com>
2023-01-30 20:38:20 +0900

    Upgrade Controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,2 +1,6 @@
 export const join = (req, res) => res.send("Join");
 export const edit = (req, res) => res.send("User Edit");
+export const remove = (req, res) => res.send("User Delete");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Log Out");
+export const see = (req, res) => res.send("See");
--- src/routes/videoRouter.js
+++ src/routes/videoRouter.js
@@ -1,5 +1,15 @@
 import express from "express";
+import {
+  watch,
+  edit,
+  deleteVideo,
+  upload,
+} from "../controller/videoController";
 
 const videoRouter = express.Router();
+videoRouter.get("/:id", watch);
+videoRouter.get(":id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
