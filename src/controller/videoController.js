b47b6277810045bec8a7f73b7048ee5ae2eb3dbe
tkdgh7063 <hoo412298@gmail.com>
2023-02-01 00:16:22 +0900

    Usage of pug conditions

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,8 +1,14 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const testUser = {
+  username: "tkdgh",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", user: testUser });
 export const watch = (req, res) =>
-  res.render("watch", { pageTitle: "Watch Video" });
+  res.render("watch", { pageTitle: "Watch Video", testUser });
 export const edit = (req, res) =>
-  res.render("edit", { pageTitle: "Edit Video" });
+  res.render("edit", { pageTitle: "Edit Video", testUser });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
