f75435cd662ba8e6c1606b7f5a79e60ef9759718
tkdgh7063 <hoo412298@gmail.com>
2023-01-31 23:14:24 +0900

    Create edit.pug, watch.pug

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,11 +1,8 @@
-export const watch = (req, res) => {
-  console.log(`${req.params.id}`);
-  return res.send("Watch");
-};
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
 export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const watch = (req, res) =>
+  res.render("watch", { pageTitle: "Watch Video" });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
