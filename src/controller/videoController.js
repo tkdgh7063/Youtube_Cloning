e282d87279d960bedf3c26a74940a5c9b0210418
tkdgh7063 <hoo412298@gmail.com>
2023-02-01 00:48:32 +0900

    Usage of pug mixins
    mixins is reusable blocks of codes that can take arguments

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,5 +1,62 @@
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "탑건: 매버릭",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "클라우스",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "그린 북",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+    {
+      title: "원더",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 4,
+    },
+    {
+      title: "덕구",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 5,
+    },
+    {
+      title: "클래식",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 6,
+    },
+    {
+      title: "나 홀로 집에",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 7,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) =>
