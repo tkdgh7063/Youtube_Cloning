7dbd1b4d45075ee17188fe453870275cace17f39
tkdgh7063 <hoo412298@gmail.com>
2023-02-01 00:26:41 +0900

    Usage of pug iteration

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,14 +1,11 @@
-const testUser = {
-  username: "tkdgh",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", user: testUser });
 export const watch = (req, res) =>
-  res.render("watch", { pageTitle: "Watch Video", testUser });
+  res.render("watch", { pageTitle: "Watch Video" });
 export const edit = (req, res) =>
-  res.render("edit", { pageTitle: "Edit Video", testUser });
+  res.render("edit", { pageTitle: "Edit Video" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
