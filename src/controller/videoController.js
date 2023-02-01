94ceb6857aa315f94eb3fc0d96a562a5cdaf59c6
tkdgh7063 <hoo412298@gmail.com>
2023-02-01 20:50:36 +0900

    Update watch.pug
    added views, comments of video and edit video link

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,66 +1,70 @@
+let videos = [
+  {
+    title: "탑건: 매버릭",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "클라우스",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "그린 북",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+  {
+    title: "원더",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 4,
+  },
+  {
+    title: "덕구",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 5,
+  },
+  {
+    title: "클래식",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 6,
+  },
+  {
+    title: "나 홀로 집에",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 7,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "탑건: 매버릭",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "클라우스",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "그린 북",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-    {
-      title: "원더",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 4,
-    },
-    {
-      title: "덕구",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 5,
-    },
-    {
-      title: "클래식",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 6,
-    },
-    {
-      title: "나 홀로 집에",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 7,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) =>
-  res.render("watch", { pageTitle: "Watch Video" });
+export const watch = (req, res) => {
+  const { id } = req.params; // const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) =>
   res.render("edit", { pageTitle: "Edit Video" });
 export const search = (req, res) => res.send("Search");
