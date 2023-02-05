e2855057b7f5a580162e16a089c80172cd0b01d2
tkdgh7063 <hoo412298@gmail.com>
2023-02-05 21:02:03 +0900

    Set database name

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/");
+mongoose.connect("mongodb://127.0.0.1:27017/youtube-clone");
 
 const db = mongoose.connection;
 
