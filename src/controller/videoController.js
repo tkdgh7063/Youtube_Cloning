ec0b3c4933fc19934339641e3c9bbf8e1ad52af5
tkdgh7063 <hoo412298@gmail.com>
2023-02-07 01:42:39 +0900

    Edit video abled
    Video model modification for empty description and hashtags.
    accordingly modified upload, edit function.

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -9,33 +9,62 @@ export const home = async (req, res) => {
     return res.render("server-error");
   }
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params; // const id = req.params.id;
   const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing: ` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description: description ? description : "",
+    hashtags: hashtags
+      ? hashtags.split(",").map((word) => {
+          word = word.trim();
+          return word.startsWith("#") ? word : `#${word}`;
+        })
+      : undefined,
+  });
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
-      description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => word.trim())
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      description: description !== "" ? description : "",
+      hashtags:
+        hashtags !== ""
+          ? hashtags.split(",").map((word) => {
+              word = word.trim();
+              return word.startsWith("#") ? word : `#${word}`;
+            })
+          : undefined,
     });
     return res.redirect("/");
   } catch (error) {
@@ -45,7 +74,9 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
 export const search = (req, res) => res.send("Search");
+
 export const deleteVideo = (req, res) => {
   return res.send("Video Delete");
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 20 },
-  description: { type: String, trim: true, maxLength: 140 },
+  description: { type: String, trim: true, maxLength: 140, default: "" },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String, trim: true }],
+  hashtags: [{ type: String, trim: true, default: undefined }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
