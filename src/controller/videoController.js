0d0a5901ef996e69a7a59ef2a90352493716036f
tkdgh7063 <hoo412298@gmail.com>
2023-02-08 03:31:17 +0900

    Update videoController.js
    added status codes when response return

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -6,7 +6,7 @@ export const home = async (req, res) => {
     const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
-    return res.render("server-error");
+    return res.status(400).render("server-error");
   }
 };
 
@@ -14,7 +14,7 @@ export const watch = async (req, res) => {
   const { id } = req.params; // const id = req.params.id;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
@@ -23,7 +23,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
@@ -33,7 +33,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -57,7 +57,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
