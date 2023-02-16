2f1f3a098a1c4052f17ca029a91e6e462633b485
tkdgh7063 <hoo412298@gmail.com>
2023-02-16 20:13:18 +0900

    Webpack branch merge

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/js/main.js", // src file from
+  mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  output: {
+    filename: "js/main.js", // src file to
+    path: path.resolve(__dirname, "assets"), // should be absolute path
+    clean: true, // clean previously created files
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
