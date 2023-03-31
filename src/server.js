c983002408198c453e3f9e459a88f1bf0a23ef08
tkdgh7063 <hoo412298@gmail.com>
2023-04-01 05:16:42 +0900

    Revert "Bug Fix"
    This reverts commit bedd9dc3b6962c8816c66178adb7cac4ce5a4170.

--- src/server.js
+++ src/server.js
@@ -31,11 +31,6 @@ app.use(
 );
 app.use(flash());
 app.use(localsMiddleware);
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
 app.use("/uploads", express.static("uploads"));
 app.use("/assets", express.static("assets"));
 app.use("/", rootRouter);
